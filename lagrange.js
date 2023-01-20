07c0d25d26ad27a10ae1cf7eabc94272ec28c848
joselor <jose.lorgere6@gmail.com>
2023-01-20 23:17:29 +0100

    Update lagrange.js
    Changement de distance avec racine carré, à une distance au carré ( équivalent )
    Mini bug : si on appuyait sur l'écran sans bouger la souris le polynôme ne se dessinait. C'est réglé mtn

--- lagrange.js
+++ lagrange.js
@@ -1,8 +1,3 @@
-<<<<<<< HEAD
-n_points = window.innerWidth / 3;
-point_radius = 5;
-click_radius = 7;
-=======
 // -----------------------------------------------------------------------------
 // Les paramètres constants sont définie dans le fichier config.js
 // 
@@ -10,7 +5,6 @@ click_radius = 7;
 // sont dans le fichier polynome.js
 //
 // -----------------------------------------------------------------------------
->>>>>>> b5846dbfbe83bc910f967cd8af7f0a265e4bdae1
 
 
 
@@ -34,13 +28,14 @@ function L(array_x, i) {
 function inter_Lagrange(array_x, array_y, x) {
 	let sumProd = [0];
 
-	for (let k = 0; k < array_x.length; k++) {
+	for (let k = 0; k < array_x.length; k++)
+	{
 		let poly = L(array_x, k);
 		poly = produit_p([
 			[array_y[k]], // Constante
 			poly
 		])
-
+		
 		sumProd = somme_p([poly, sumProd]); // On ajoute tout les polynome (on en fait qu'un)
 	}
 
@@ -71,22 +66,13 @@ function draw_function(f, dx) {
 	}
 }
 
-<<<<<<< HEAD
-function eval_and_draw(f, dx)
-{
-    f = [];
-    for (let i = 0; i < n_points; i++) {
-        let val = inter_Lagrange(xi, yi, dx[i]);
-        f.push(val);
-    }
-=======
 // Permet de calculer et d'afficher la fonction
 function eval_and_draw(f, dx) {
+	f = []
 	for (let i = 0; i < N_POINTS; i++) {
 		let val = inter_Lagrange(xi, yi, dx[i]);
 		f.push(val);
 	}
->>>>>>> b5846dbfbe83bc910f967cd8af7f0a265e4bdae1
 
 	ctx.strokeStyle = "white";
 	draw_function(f, dx);
@@ -108,22 +94,6 @@ function generate_dx() {
 	return out;
 }
 
-//Verifie si (x, y) est à une distance inférieure à distance d'un des points
-function point_proche(x, y, array_x, array_y, distance)
-{
-    for (let i = 0; i < array_x.length; i++)
-    {
-        //On calcule la distance au carré
-        let dist_x = x - array_x[i];
-        let dist_y = y - array_y[i];
-        let sqrdist = dist_x*dist_x + dist_y*dist_y;
-
-        if (sqrdist <= distance*distance)
-            return i;
-    }
-    return -1;
-}
-
 var canvas = document.getElementById('canvas');
 var ctx = canvas.getContext('2d');
 
@@ -137,29 +107,21 @@ let dx = generate_dx();
 let held_point = -1;
 let is_holding = false;
 
-function distance_euclidienne(x1, y1, x2, y2) {
-    return Math.sqrt((x1 - x2) ** 2 + (y1 - y2) ** 2);
+function distance_carre(x1, y1, x2, y2) {
+    return (x1 - x2) ** 2 + (y1 - y2) ** 2;
 }
 
 window.onmousedown = e => {
-<<<<<<< HEAD
-    //Permet de supprimer un point en cliquant dessus
-    let i = point_proche(e.x, window.innerHeight - e.y, xi, yi, click_radius);
-    if (i != -1)
-    {
-        xi.splice(i, 1);
-        yi.splice(i, 1);
-        eval_and_draw(f, dx);
-    }
-    else if (!xi.includes(e.x)) {
-=======
-    let n = xi.length;
+	let n = xi.length;
 
     // attraper un point déjà posé
     for(let i = 0; i < n; i++) {
-        let d = distance_euclidienne(xi[i], yi[i], e.x, window.innerHeight - e.y);
-        console.log(d);
-        if (d < POINT_RADIUS && !xi.includes(e.x)) {
+		//La distance au carré est moins couteuse à calculer ( la racine carré c long )
+		//En plus on a puisque le carré est strictement croissant sur des positifs
+		//sqrt(d) < POINT_RADIUS <=> d < POINT_RADIUS**2
+        let d = distance_carre(xi[i], yi[i], e.x, window.innerHeight - e.y);
+		console.log(d);
+        if (d <= CLICK_RADIUS ** 2 && !xi.includes(e.x)) {
             held_point = i;
             is_holding = true;
             console.log("je prend le controle de " + i);
@@ -168,44 +130,29 @@ window.onmousedown = e => {
 
 
     if (!is_holding && !xi.includes(e.x)) {
->>>>>>> b5846dbfbe83bc910f967cd8af7f0a265e4bdae1
         is_holding = true;
         held_point = xi.length;
         xi.push(e.x);
         yi.push(window.innerHeight - e.y);
+		eval_and_draw(f, dx);
     }
 }
 
 window.onmousemove = e => {
-<<<<<<< HEAD
-    if (is_holding)
-    {
-        point(e.x, e.y);
-
-        xi[held_point] = e.x;
-        yi[held_point] = window.innerHeight - e.y;
-        eval_and_draw(f, dx);
-    }
-=======
 	if (held_point != -1 && is_holding) {
-		f = []
 		point(e.x, e.y);
 
 		xi[held_point] = e.x;
 		yi[held_point] = window.innerHeight - e.y;
-		f = []
 		eval_and_draw(f, dx);
 	}
->>>>>>> b5846dbfbe83bc910f967cd8af7f0a265e4bdae1
 }
 
 window.onmouseup = e => {
-
     if (!is_holding) {
         return false;
     }
 
     held_point = -1;
     is_holding = false;
-    eval_and_draw(f, dx);
 }
