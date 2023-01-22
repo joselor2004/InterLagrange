c498a0dab978be7b62390cbc87fd9c4606c494b5
Faywynnn <albanlr2004@gmail.com>
2023-01-22 21:33:26 +0100

    Update panel.js

--- js/panel.js
+++ js/panel.js
@@ -1,10 +1,21 @@
 
 
 function moveNav() {
-	console.log(PANELOPEN, PANEL_WIDTH)
 	document.getElementById("panel").style.width = PANELOPEN ? "0px" : PANEL_WIDTH + "px";
 	document.getElementById('fonction').style.width = (PANEL_WIDTH - 20) + "px";
-	document.querySelector('.panelButton').style.left = PANELOPEN ? "20px" : PANEL_WIDTH + 20 + "px";
+	document.querySelector('.panelButton').style.left = PANELOPEN ?
+	"20px" :
+	window.innerWidth > 400 ?
+	PANEL_WIDTH + 20 + "px" :
+	window.innerWidth - 70 + "px";
+
+	// if (PANELOPEN && window.innerWidth <= 400) {
+	// 	document.getElementsByClassName('line-menu').foreach((e) => {
+	// 		e.style.color = "black";
+	// 		console.log(e)
+	// 	});
+	// }
+
 	document.querySelector('.panelButton').classList.toggle('open');
 
 
